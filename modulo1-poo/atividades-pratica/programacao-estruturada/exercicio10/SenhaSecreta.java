025025a0384de4313140498df6db9a58f6f2d6ca
JoãoPedrinho013 <135490211+JoaoPedrinho013@users.noreply.github.com>
2025-08-12 19:00:51 -0300

    Update SenhaSecreta.java

--- modulo1-poo/atividades-pratica/programacao-estruturada/exercicio10/SenhaSecreta.java
+++ modulo1-poo/atividades-pratica/programacao-estruturada/exercicio10/SenhaSecreta.java
@@ -1,6 +1,28 @@
 // import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class SenhaSecreta {
-    String senhaScreta = "PaoComOvo123";
-    
+    public static String validacao() {
+        String senhaScreta = "PaoComOvo123", senha;
+        Boolean verificador = false;
+        Scanner scan = new Scanner(System.in);
+
+        while (verificador != true) {
+            System.out.print("Digite a senha para logar: ");
+            senha = scan.nextLine();
+            if (senha.equals(senhaScreta)){
+                verificador = true;
+            } else {
+                System.out.println("Senha inválida, tente novamente.");
+            }
+        }
+        return "Parabéns você logou!";
+    }
+
+    public static void main(String[] args) {
+        String logar = validacao();
+        System.out.println(logar);
+    }
+
 }
