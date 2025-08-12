4b6041b95ff3dd1d703a45e271d56f2334e1daf9
JoãoPedrinho013 <135490211+JoaoPedrinho013@users.noreply.github.com>
2025-08-12 16:10:12 -0300

    Create ListaNome.java

---
+++ modulo1-poo/atividades-pratica/programacao-estruturada/exercicio09/ListaNome.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+
+public class ListaNome {
+
+        
+    public static String[] addNames() {
+        Scanner scan = new Scanner(System.in);
+        String[] nomes = new String[5];
+        for(int i = 0; i < 5; i++) {
+            System.out.print("Digite o " + (i + 1) + "º nome: ");
+            nomes[i] = scan.nextLine().toLowerCase();
+        }
+        System.out.println("------------------------------");
+        System.out.println("Nomes adcionados!");
+        System.out.println("------------------------------");
+        return nomes;
+    }
+    
+    public static void verificarName() {
+        Scanner scan = new Scanner(System.in);
+        String n;
+        int somador=0;
+        String[] lista = addNames();
+
+        System.out.println("Digite um nome para ver se está junto com os demais: ");
+        n = scan.nextLine();
+        
+        for (String nome : lista) {
+            if(nome.equals(n.toLowerCase())) {
+                somador += 1;
+            }
+        }
+
+        if (somador != 0){
+            System.out.println("Existe esse nome na Lista!");
+        } else {
+            System.out.println("Não Existe esse nome na Lista!");
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        verificarName();
+    }
+}
