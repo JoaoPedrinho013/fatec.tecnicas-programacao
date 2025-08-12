0345a4e0e7d780098679c00832d441f3279f517c
JoãoPedrinho013 <135490211+JoaoPedrinho013@users.noreply.github.com>
2025-08-12 14:15:51 -0300

    Update Fatorial.java

--- modulo1-poo/atividades-pratica/programacao-estruturada/exercicio06/Fatorial.java
+++ modulo1-poo/atividades-pratica/programacao-estruturada/exercicio06/Fatorial.java
@@ -7,7 +7,12 @@ public class Fatorial {
 
         System.out.print("Digite um número para calcular o Fatorial: ");
         num = scan.nextInt();
-
+        // 5! = 5 x 4 x 3 x 2 x 1
+        int somador = 1;
+        for(int i = 2; i <= num; i++){
+            somador *= i;
+        }
+        System.out.println("O fatorial de "+ num + " é " + somador);
         
     }
 }
