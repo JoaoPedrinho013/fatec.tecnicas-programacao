31b4d5fb95904818213dc069710e06402d62356b
JoãoPedrinho013 <135490211+JoaoPedrinho013@users.noreply.github.com>
2025-08-15 17:27:07 -0300

    .

--- modulo1-poo/atividades-pratica/repeticao-matriz/exercicio02/verificarLogin.java
+++ modulo1-poo/atividades-pratica/repeticao-matriz/exercicio02/verificarLogin.java
@@ -1,5 +1,3 @@
-package exercicio02;
-
 import java.util.Scanner;
 
 public class verificarLogin {
@@ -14,14 +12,26 @@ public class verificarLogin {
             {"Edu", "4312"},
             {"Pedro", "3241"}
         };
+
         System.out.print("Digite seu login: ");
         login = scan.nextLine();
 
         System.out.print("Digite sua senha: ");
         senha = scan.nextLine();
 
-        
+        boolean logou = false;
 
+        for (int l = 0; l < loginSenhas.length; l++) {
+            if (login.equals(loginSenhas[l][0]) && senha.equals(loginSenhas[l][1])) {
+                logou = true;
+                break;
+            }
+        }
 
+        if (logou) {
+            System.out.println("Logou!");
+        } else {
+            System.out.println("Não logou.");
+        }
     }
 }
---
+++ modulo1-poo/atividades-pratica/repeticao-matriz/exercicio03/transposicaoMatriz.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class transposicaoMatriz {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int[][] matriz = new int[3][3];
+    int[][] matrizTrocada = new int[3][3];
+
+    for(int l = 0; l < 3; l++){
+        for(int c = 0; c < 3; c++){
+            System.out.print("Add na linha " + l + " e coluna " + c + ": ");
+            matriz[l][c] = scan.nextInt();
+        }
+    }
+    for(int l = 0; l < 3; l++){
+        for(int c = 0; c < 3; c++){
+            matrizTrocada[l][c] = matriz[c][l];
+            System.out.print(matrizTrocada[l][c] + " ");
+        }
+        System.out.println(" ");
+    }
+  }  
+}
